782972766aff9f92c289ea341b43821763f85dc0
kirilvcs <37610195+kirilvcs@users.noreply.github.com>
2020-02-27 10:28:38 +0200

    Initial commit

---
+++ custom.js
@@ -0,0 +1,40 @@
+"use strict";
+
+console.log("JS task 1");
+
+// Įkelkite tik javascript failo turinį
+
+// 1. Turite kintamaji kuriame saugoma temperatura Farenheitu
+let tempF = 69; //Temperatura gali keistis, kodas turetu veikti visada
+// parasykite koda kuris sukonvertuotu temperatura i Celsiju
+// Atsakymas matomas console: 20.5555555555555555
+
+
+// 1.1 Apribokite, kad rodytu tik 1 skaiciu po kablelio
+// Atsakymas matomas console: 20.6
+// BONUS - padarykite, kad neapvalintu. Atsakymas matomas console: 20.5
+
+
+// 1.2  Turite kintamaji su tekstu:
+let text1 = "Lietuvoje šiuo metu laipsniai pagal Celsijų";
+// Naudojant replace suformuokite sakini, kuriame bus iterpta temperatura
+// Atsakymas matomas console: Lietuvoje šiuo metu 20.6 laipsniai pagal Celsijų
+
+
+// 1.3  Turite kintamaji su tekstu:
+let text2 = "Lietuvoje šiuo metu laipsniai pagal Celsijų";
+// Naudojant substring suformuokite sakini, kuriame bus iterpta temperatura
+// Atsakymas matomas console: Lietuvoje šiuo metu 20.6 laipsniai pagal Celsijų
+// "Lietuvoje šiuo metu laipsniai pagal Celsijų"
+//  012345678901234567890
+
+// 2. Gauti tik skaicius kurie eina po kablelio
+//	  Nepamirstame, kad skaicius gali pasikeisti
+//	  Isvedime nurodome kiek skaiciu yra po kablelio
+//      ir kokie tie skaiciai
+//      *gaunamas visada skaicius pas kuri yra skaiciai po kablelio
+
+let skaicius = 12.235346547;
+
+// Rezultatas console:
+// Skaiciu po kablelio: 9, jie yra: 235346547
